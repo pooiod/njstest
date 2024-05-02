dd0ef478a3697693a0fdb88173fcdc8b28015801
pooiod7 <82846639+pooiod@users.noreply.github.com>
2024-05-02 18:15:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3111;
 
 app.get('/', (req, res) => {
-    res.send('Welcome to a WebContainers app!');
+    res.send(`Welcome to a WebContainers app! <script>window.parent.postMessage({js:"alert('hello');"}, '*');</script>`);
 });
 
 app.listen(port, () => {
